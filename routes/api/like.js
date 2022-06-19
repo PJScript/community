eb4f2635830ec1018982f090dd6b1b1b938832ea
PJScript <kilo718@naver.com>
2022-06-19 20:26:08 +0900

    fix

--- routes/api/like.js
+++ routes/api/like.js
@@ -45,51 +45,31 @@ router.post('/:id', (req, res, next) => {
         Board.findOne({ _id: id }).then((boardData) => {
             let like = boardData.like
             let like_count = boardData.like_count
+            let likeBoolean = false
+            like = like.filter((el, idx) => {
+                if (el.email === email) {
+                    likeBoolean = true
+                } else {
+                    return el
+                }
+            })
+            console.log(like, "라이크")
 
-            if (like.length === 0) {
-                like_count += 1;
-                Boolean = true
-
-                like = [{
+            if (likeboolean === false) {
+                like.push({
                     usr_name: name,
                     email: email,
                     created_at: Date.now(),
                     updated_at: Date.now(),
                     deleted_at: null,
-                }]
-            } else {
-                
-                for(let i=0; i<=like.length-1; i++){
-                   if(like[i].email === email){
-                    like = like.filter((el,idx)=>{
-                      return el.email !== email
-                    })
-                     break;
-                   }else{
-                    like.push(
-                        {
-                            usr_name: name,
-                            email: email,
-                            created_at: Date.now(),
-                            updated_at: Date.now(),
-                            deleted_at: null,
-                        }
-                    )
-                   }
-                }
+                })  
+            } 
 
-                
 
-                // if (like.length === 0) {
-                //     like_count -= 1;
-                //     Boolean = false
-                // }
-
-            }
             Board.findOneAndUpdate({ _id: id }, { like: like, like_count: like_count }, {
                 new: true
             }).then((data) => {
-                res.status(200).send({result:Boolean})
+                res.status(200).send({ result: Boolean })
                 return;
             })
             // console.log(boardData)
